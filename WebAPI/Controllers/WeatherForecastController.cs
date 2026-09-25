d3d5acbbf3f398c90aafcea09bfa44d945818589
Brian Barrett <bbarrett@theopalgroup.com>
2026-09-25 15:31:06 -0400

    Add api project

---
+++ WebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class WeatherForecastController : ControllerBase
+  {
+    private static readonly string[] Summaries =
+    [
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    ];
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+      return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+      {
+        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        TemperatureC = Random.Shared.Next(-20, 55),
+        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+      })
+      .ToArray();
+    }
+
+    [HttpPost(Name = "GetWeatherForecast")]
+    public string GetLocation(string location)
+    {
+      try
+      {
+        var intLocation = int.Parse(location);
+      }
+      catch (Exception ex)
+      {
+        return ex.ToString();
+      }
+
+      return System.Text.Json.JsonSerializer.Serialize(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+      {
+        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        TemperatureC = Random.Shared.Next(-20, 55),
+        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+      })
+      .ToArray());
+    }
+  }
+}
